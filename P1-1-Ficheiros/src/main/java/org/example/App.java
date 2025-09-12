6e455759ff055df67980603df44542e73d109a82
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-09-12 13:25:23 +0200

    ejercico 2 de la parte 2, terminado y funciona correctamente

--- P1-1-Ficheiros/src/main/java/org/example/App.java
+++ P1-1-Ficheiros/src/main/java/org/example/App.java
@@ -4,11 +4,15 @@ public class App {
     static String ficheroAbsoluto;
     static String arquivosdir = "/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir";
     public static void main(String[] args) {
-        ejercicio1();
+        ejercicio2();
     }
     public static void ejercicio1(){
         TratamientoDeFicheros.crearDirectorio(arquivosdir);
         System.out.println(TratamientoDeFicheros.eDirectorio(arquivosdir));
+    }
+    public static void ejercicio2(){
+        TratamientoDeFicheros.crearFichero(arquivosdir,"Products1.txt");
+        System.out.printf(TratamientoDeFicheros.eFichero("/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir/Products1.txt"));
     }
 
 }
