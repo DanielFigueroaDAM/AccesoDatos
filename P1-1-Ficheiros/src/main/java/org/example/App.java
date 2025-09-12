ebb2c7f95e1cae828476f946a98213421156fb5d
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-09-12 13:45:10 +0200

    ejercico 3 de la parte 2, terminado y funciona correctamente

--- P1-1-Ficheiros/src/main/java/org/example/App.java
+++ P1-1-Ficheiros/src/main/java/org/example/App.java
@@ -1,10 +1,13 @@
 package org.example;
 
 public class App {
-    static String ficheroAbsoluto;
+    static String products1_txt = "/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir/Products1.txt";
     static String arquivosdir = "/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir";
+    static String subdir = "/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir/subdir";
+    static String products2_txt = "/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir/subdir/Products2.txt";
+
     public static void main(String[] args) {
-        ejercicio2();
+        ejercicio3();
     }
     public static void ejercicio1(){
         TratamientoDeFicheros.crearDirectorio(arquivosdir);
@@ -12,7 +15,14 @@ public class App {
     }
     public static void ejercicio2(){
         TratamientoDeFicheros.crearFichero(arquivosdir,"Products1.txt");
-        System.out.printf(TratamientoDeFicheros.eFichero("/home/dam/IdeaProjects/AccesoDatos/P1-1-Ficheiros/src/arquivosdir/Products1.txt"));
+        System.out.printf(TratamientoDeFicheros.eFichero(products1_txt));
+    }
+    public static void ejercicio3(){
+        System.out.println(TratamientoDeFicheros.crearDirectorio(subdir));
+        System.out.println(TratamientoDeFicheros.crearFichero(subdir,"product2_txt"));
+    }
+    public static void ejercicio4(){
+
     }
 
 }
