fdb450da05aff190962093c3d39d5c4f8eee32a3
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-09-22 18:46:49 +0200

    parte 2 terminada

--- P1-2_I_OStream/src/main/java/org/example/Main.java
+++ P1-2_I_OStream/src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TratamientoDeFicheros2.escribirFicheroAFichero("texto1.txt", "texto2.txt");
         //TratamientoDeFicheros2.añadirFicheroAFichero("texto1.txt", "texto2.txt");
-        //TratamientoDeFicheros2.escribirFicheroAFichero("paloma1.jpg", "periquito1.jpg");
-        TratamientoDeFicheros2.añadirFicheroAFichero("paloma1.jpg", "periquito1.jpg");
+        //
+        // TratamientoDeFicheros2.escribirFicheroAFichero("paloma1.jpg", "periquito1.jpg");
+        //TratamientoDeFicheros2.añadirFicheroAFichero("paloma1.jpg", "periquito1.jpg");
+        //TratamientoDeFicheros3.escribirFicheroAFicheroBuffered("texto1.txt", "texto2.txt");
     }
 }
