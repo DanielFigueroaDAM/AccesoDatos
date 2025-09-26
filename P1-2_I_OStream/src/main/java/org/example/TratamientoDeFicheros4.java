58fd8ae95dafd44af583341caa6a56c3adad1b56
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-09-26 12:35:10 +0200

    arreglado tema file size

--- P1-2_I_OStream/src/main/java/org/example/TratamientoDeFicheros4.java
+++ P1-2_I_OStream/src/main/java/org/example/TratamientoDeFicheros4.java
@@ -7,13 +7,14 @@ public class TratamientoDeFicheros4 {
     public static void escribiendoCadenas(String cadena, String archivo){
         DataOutputStream salida = null;
         try {
+            File archivoF = new File(archivo);
             FileOutputStream fichero;
-            salida = new DataOutputStream(fichero = new FileOutputStream(archivo, true));
+            salida = new DataOutputStream(fichero = new FileOutputStream(archivoF, true));
             salida.writeUTF(cadena);
             System.out.println("Escribiendo la cadena: "+cadena);
-            System.out.println("Tamaño del fichero: "+fichero.()); // esto no va, preguntar al profesor como funciona¿clase File?
+            System.out.println("Tamaño del fichero: "+fichero.getChannel().size());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println( "Error E/S: " + e.getMessage() );
         } finally {
             if (salida != null) {
                 try {
