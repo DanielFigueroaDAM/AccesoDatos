38f1979dfa0677a3b9e4dc8100b4062e4f7deef8
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-10-08 14:15:23 +0200

    iniciando xml

---
+++ Practica-1-3-IO-Serializacion-e-XML/src/main/java/crearxml/App.java
@@ -0,0 +1,7 @@
+package crearxml;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Practica-1-3-IO-Serializacion-e-XML/src/main/java/crearxml/GeneradorXML.java
@@ -0,0 +1,14 @@
+package crearxml;
+
+import
+
+import javax.xml.parsers.SAXParser;
+
+public class GeneradorXML {
+
+
+    public static void generadorCabecera(){
+
+
+    }
+}
